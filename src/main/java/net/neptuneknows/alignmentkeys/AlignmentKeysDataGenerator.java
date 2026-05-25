f3fb52d882790fbd775db975362b50451fcd8c3a
rafael <neptuneknow@gmail.com>
2026-05-25 14:41:35 +0100

    Initial commit

---
+++ src/main/java/net/neptuneknows/alignmentkeys/AlignmentKeysDataGenerator.java
@@ -0,0 +1,11 @@
+package net.neptuneknows.alignmentkeys;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AlignmentKeysDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
