1a54ad17fca658e4d09694695a4e9bdac4c1657b
rafael <neptuneknow@gmail.com>
2026-05-25 16:24:57 +0100

    Added alignment keys for yaw and pitch.

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Alignment Keys
 
-## Setup
+This mod can be used to align yourself. This is useful for specific stuff. 
 
-For setup instructions, please see the [Fabric Documentation page](https://docs.fabricmc.net/develop/getting-started/creating-a-project#setting-up) related to the IDE that you are using.
-
-## License
-
-This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+This is available under the MIT licence.
--- src/main/java/net/neptuneknows/alignmentkeys/AlignmentKeys.java
+++ src/main/java/net/neptuneknows/alignmentkeys/AlignmentKeys.java
@@ -1,24 +1,96 @@
 package net.neptuneknows.alignmentkeys;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
+import org.lwjgl.glfw.GLFW;
+import net.minecraft.client.Minecraft;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class AlignmentKeys implements ModInitializer {
 	public static final String MOD_ID = "alignmentkeys";
+	KeyMapping.Category CATEGORY = KeyMapping.Category.register(
+			Identifier.fromNamespaceAndPath(AlignmentKeys.MOD_ID, "custom_category")
+	);
+	float snapAngleYaw45 = 45.0f;
+	float snapAnglePitch1 = 1.0f;
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	KeyMapping clockwiseRotation = KeyMappingHelper.registerKeyMapping(
+			new KeyMapping(
+					"Rotate 45° Clockwise",
+					InputConstants.Type.KEYSYM,
+					GLFW.GLFW_KEY_PERIOD,
+					this.CATEGORY
+			));
+	KeyMapping counterClockwiseRotation = KeyMappingHelper.registerKeyMapping(
+			new KeyMapping(
+					"Rotate Yaw 45° Counterclockwise",
+					InputConstants.Type.KEYSYM,
+					GLFW.GLFW_KEY_COMMA,
+					this.CATEGORY
+			));
+	KeyMapping rotatePitchUp = KeyMappingHelper.registerKeyMapping(
+			new KeyMapping(
+					"Rotate Pitch 1° Up",
+					InputConstants.Type.KEYSYM,
+					GLFW.GLFW_KEY_UP,
+					this.CATEGORY
+			));
+	KeyMapping rotatePitchDown = KeyMappingHelper.registerKeyMapping(
+			new KeyMapping(
+				"Rotate Pitch 1° Down",
+				InputConstants.Type.KEYSYM,
+				GLFW.GLFW_KEY_DOWN,
+				this.CATEGORY
+			));
+	public void rotateYaw45(Minecraft client, int direction) {
+		if (client.player == null) return;
+
+		float yaw = client.player.getYRot();
+		yaw = (yaw % 360 + 360) % 360;
+
+		float snap = Math.round(yaw / snapAngleYaw45) * snapAngleYaw45;
+		snap += snapAngleYaw45 * direction;
+
+		if (snap >= 360) snap -= 360;
+		if (snap < 0) snap += 360;
+
+		client.player.setYRot(snap);
+	}
+	public void rotatePitch1(Minecraft client, int direction) {
+		if (client.player == null) return;
+
+		float pitch = client.player.getXRot();
+		pitch = (pitch % 360 + 360) % 360;
+
+		float snap = Math.round(pitch / snapAnglePitch1) * snapAnglePitch1;
+		snap += snapAnglePitch1 * direction;
+
+		if (snap >= 360) snap -= 360;
+		if (snap < 0) snap += 360;
+
+		client.player.setXRot(snap);
+	}
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while(clockwiseRotation.consumeClick()) {
+				rotateYaw45(client, 1);
+			}
+			while(counterClockwiseRotation.consumeClick()) {
+				rotateYaw45(client, -1);
+			}
+			while(rotatePitchUp.consumeClick()) {
+				rotatePitch1(client, -1);
+			}
+			while(rotatePitchDown.consumeClick()) {
+				rotatePitch1(client, 1);
+			}
+		});
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
